317c7df81abd7616087ab75d6f80e99f6f082602
John Swanson <john@swansonwriting.com>
2022-03-17 14:30:23 -0400

    Consistency pass on big-card css

--- frontend/src/components/big-card.jsx
+++ frontend/src/components/big-card.jsx
@@ -12,31 +12,33 @@ class BigCard extends React.Component {
 
     render() {
         return(
-            <div id='container'>
-                <div id='background'>
-                    <div className='row' id='img-carousel'> 
-                        Image Carousel goes here
-                    </div>
+            <div id='flex-container'>
+                <div id='container'>
+                    <div id='background'>
+                        <div className='row' id='img-carousel'> 
+                            Image Carousel goes here
+                        </div>
 
-                    <div className='row'  id='about-me'>
-                        <h1>About Me</h1>
-                        User data here
-                    </div>
+                        <div className='row'  id='about-me'>
+                            <h1>About Me</h1>
+                            User data here
+                        </div>
 
-                    <div className='row' id='socials'>
+                        <div className='row' id='socials'>
+                            <ul>
+                                <i class="bi bi-github"></i>
+                                <i class="bi bi-linkedin"></i>
+                                <i class="bi bi-file-person-fill"></i>
+                            </ul>
+                        </div>
+                    </div>
+                    <div id="footer" className='row'>
                         <ul>
-                            <i class="bi bi-github"></i>
-                            <i class="bi bi-linkedin"></i>
-                            <i class="bi bi-file-person-fill"></i>
+                            <i class="bi bi-hand-thumbs-up-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-hand-thumbs-down-fill"></i>
                         </ul>
                     </div>
-                </div>
-                <div id="footer" className='row'>
-                    <ul>
-                        <i class="bi bi-hand-thumbs-up-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-hand-thumbs-down-fill"></i>
-                    </ul>
                 </div>
             </div>
         );
