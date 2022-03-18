9142af51b981097a51d64536cfed6b9262ed9d52
John Swanson <john@swansonwriting.com>
2022-03-18 14:12:29 -0400

    Changed testpage to show cards to allow for iframe testing

--- frontend/src/pages/test/test.jsx
+++ frontend/src/pages/test/test.jsx
@@ -2,12 +2,14 @@ import './test.css';
 import React from 'react';
 
 import ProfileCard from '../../components/profile-card/profile-card';
+import IFrameCard from '../../components/iframe-card/iframe-card';
 
 class Test extends React.Component {
   render() {
     return (
-      <div id="content">
-        <ProfileCard />
+      <div id="test-content" className='row'>
+        <div className='col'> <ProfileCard /> </div>
+        <div className='col'> <IFrameCard url='https://github.com/'/> </div>
       </div>
     );
   }
