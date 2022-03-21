75f7516dbb389eb69349635f64e422255a6fc11e
Ishan Patel <ip5038@gmail.com>
2022-03-21 16:34:46 -0400

    added form validation

--- frontend/src/components/input-form/input-form.jsx
+++ frontend/src/components/input-form/input-form.jsx
@@ -7,19 +7,34 @@ class InputForm extends React.Component {
     super(props);
 
     // Setup the state to map each input to its current value
-    let _state = {};
+    let _state = {
+      validPrimaryButton: false
+    };
     for (const input of props.inputs) {
       _state[input] = '';
     }
-
+    
     this.state = _state;
   }
 
   handleChange = (event) => {
     const { id, value } = event.target;
+
     this.setState({
+      validPrimaryButton: true,
       [id]: value,
+    }, () => {
+      for(var i = 0; i < this.props.inputs.length; i++) {
+        console.log(this.props.inputs[i]) //': ' + this.state[this.props.inputs[i]])
+        if(this.state[this.props.inputs[i]] === '') {
+          this.setState({ 
+            validPrimaryButton: false
+          })
+          break
+        }
+      }      
     });
+
   };
 
   render() {
@@ -45,9 +60,14 @@ class InputForm extends React.Component {
       }
     }
 
+    var onSignUp = false
+    if(this.props.buttons.length === 1) {
+      onSignUp = true
+    }
+
     // Creates markup for buttons
     const buttons = this.props.buttons.map((button) => (
-      <button id={button.name} onClick={onclickGen(button.callback)} key={button.name}>
+      <button id={button.name} onClick={onclickGen(button.callback)} key={button.name} disabled={(onSignUp ? !(this.state.validPrimaryButton) : false) || (button.name === 'Login' ?  !(this.state.validPrimaryButton) : false)}>
         {button.name}
       </button>
     ));
