70c1e90e04a3adb7f1ad9a0ed786ac863fcdfaad
Jonathan Swanson <john@swansonwriting.com>
2022-03-23 14:25:33 -0400

    Listing page cna now dispaly all or only relevant listings

--- frontend/src/pages/feed/feed.jsx
+++ frontend/src/pages/feed/feed.jsx
@@ -1,7 +1,7 @@
 import './feed.css';
 import React from 'react';
 import axios from 'axios'
-
+import * as _ from 'lodash';
 class Feed extends React.Component {
   state = { feeds: [] };
 
@@ -9,18 +9,39 @@ class Feed extends React.Component {
     axios.get('http://localhost:4201/listing').then(r => {
       const listings = r.data.listings;
       this.setState({
-        feeds: listings
+        feeds: listings,
+        showAll: false,
+        user: this.props.user
       })
-      console.log(listings);
     });
     /* const resp = await fetch('https://picsum.photos/v2/list');
     const feeds = await resp.json();
     this.setState({  }); */
   }
 
+  filter = () => {
+    const u = this.state.user;
+    if(this.state.showAll) {
+      return this.state.feeds;
+    } else {
+      return this.state.feeds.filter(feed => {
+        return _.intersection(feed.skills, u.skills).length > 0;
+      });
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      showAll: !this.state.showAll
+    })
+  }
+
+
   render() {
     //<img src={f.download_url} alt={f.author} />
-    const feed = this.state.feeds.map((f) => (
+    const toDisplay = this.filter(this.state.feeds);
+
+    const feed = toDisplay.map((f) => (
       <div className="entry" key={f.download_url}>
         <h4> {f.name} </h4>
         <div className="image-container">
@@ -36,6 +57,8 @@ class Feed extends React.Component {
     return (
       <div id="feed-container">
         <h1 className="title"> Hyperlink </h1>
+        <label> <input type="radio" value="showAll" checked={this.state.showAll} onChange={this.toggle} /> Show All? </label>
+        <label> <input type="radio" value="showMine" checked={!this.state.showAll} onChange={this.toggle} /> Show Related? </label>
         {feed}
       </div>
     );
