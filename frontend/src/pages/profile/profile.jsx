679416f37cc270cba39cef4de25071f547d972cf
Jonathan Swanson <john@swansonwriting.com>
2022-03-25 11:13:54 -0400

    Resolved issue with modifying user profiles

--- frontend/src/pages/profile/profile.jsx
+++ frontend/src/pages/profile/profile.jsx
@@ -11,24 +11,15 @@ class Profile extends React.Component {
         this.employeeFields = ['name', 'username', 'password', 'skills'];
         this.employeeTypes = ['text', 'text', 'password', 'text'];
 
-        this.initialUser = this.props.user;
-        if(this.initialUser) {
-            this.initialUser = {
-                ...this.initialUser,
-                skills: this.initialUser.skills.join(' ')
-            }
-        }
-
-        this.form = createRef()
+        this.form = createRef();
         this.state = {
-            user: this.initialUser || null
+            user: this.props.user || null
         };
     }
 
     static getDerivedStateFromProps = (nextProps) => {
         return({
-            user: nextProps.user,
-            skills: nextProps.user.skills.join(' ')
+            user: nextProps.user
         });  
     }
 
@@ -37,11 +28,14 @@ class Profile extends React.Component {
     modifyPressed = () => { 
         const form = this.form.current;
         const oldUser = this.state.user;
+        console.log(form.state.user.skills);
         const newUser = {
             ...oldUser,
             ...form.state.user,
-            skills: form.state.user.skills.split(' ') || []
+            skills: form.state.user.skills.split(' ')
         };
+
+        console.log(newUser);
 
         axios.put(process.env.REACT_APP_BACKEND_URL + '/user', {old: oldUser, new: newUser}).then(res => {
             this.props.login(res.data.user || {});
@@ -57,13 +51,18 @@ class Profile extends React.Component {
     }
 
     render() {
+        console.log(this.state.user);
+        const user = {
+            ...this.state.user,
+            skills: this.state.user.skills.join(' ')
+        };
         return(
             <div id='profile'>
             <h3>User Profile</h3>
             <InputForm
                 inputs={this.isEmployee() ? this.employeeFields : this.employerFields}
                 types={this.isEmployee() ? this.employeeTypes : this.employerTypes}
-                values={this.state.user}
+                values={user}
                 buttons={[{ name: 'Modify Profile', callback: this.modifyPressed },
                             { name: 'Delete Account', callback: this.deletePressed }]}
                 ref={this.form} 
