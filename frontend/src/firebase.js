baf3839e263134f44ea55a02ffe3dddc0ee59afc
Ishan Patel <ip5038@gmail.com>
2022-03-25 11:40:27 -0400

    added firebase

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,42 +1,34 @@
-// import { initializeApp } from 'firebase/app';
-// import {
-//     GoogleAuthProvider,
-//     getAuth,
-//     signInWithEmailAndPassword,
-//     createUserWithEmailAndPassword
-// } from 'firebase/auth'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 
-// // TODO: Replace the following with your app's Firebase project configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCttgfYfTcFvh9F83ByvcnHgdXIKxEE5jw",
-//     authDomain: "hyperlink-18f74.firebaseapp.com",
-//     projectId: "hyperlink-18f74",
-//     storageBucket: "hyperlink-18f74.appspot.com",
-//     messagingSenderId: "821909883789",
-//     appId: "1:821909883789:web:823057ae78860cb14e0bc4"
-// };
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
+} from 'firebase/auth'
 
-// const app = initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzDRL1JYzEojmChq5FRPZwml6Y4Tbp1-M",
+  authDomain: "hyperlink-5987b.firebaseapp.com",
+  projectId: "hyperlink-5987b",
+  storageBucket: "hyperlink-5987b.appspot.com",
+  messagingSenderId: "550095004421",
+  appId: "1:550095004421:web:1e05d282fad4c913a5cd6c"
+};
 
-// const logInWithEmailAndPassword = async (email, password) => {
-//     console.log('try sigin ')
-//     try {
-//       await signInWithEmailAndPassword(getAuth(app), email, password);
-//       console.log('logged in')
-//     } catch (err) {
-//       console.error(err);
-//       alert(err.message);
-//     }
-// };
-// export default logInWithEmailAndPassword;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-
-// // import {
-//   // ​​  GoogleAuthProvider,
-//   // ​​  getAuth,
-//   // signInWithPopup,
-//   // ​​  signInWithEmailAndPassword,
-//   // ​​  createUserWithEmailAndPassword,
-//   // ​​  sendPasswordResetEmail,
-//   // ​​  signOut,
-//   // ​​} from "firebase/auth";
+const logInWithEmailAndPassword = async (email, password) => {
+    console.log('try sigin ')
+    try {
+      await signInWithEmailAndPassword(getAuth(app), email, password);
+      console.log('logged in')
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+};
+export default logInWithEmailAndPassword;
