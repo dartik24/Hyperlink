5ecc7f0989a02c1fd54973f6b2ed329a5a34023b
Ishan Patel <ip5038@gmail.com>
2022-04-05 19:43:58 -0400

    fixed hardcoded getCollection

--- frontend/src/firebase/fb-generic.js
+++ frontend/src/firebase/fb-generic.js
@@ -3,18 +3,14 @@ import firebase from './firebase'
 
 // getLike is used to get only listings that user has liked. Pass true for parameter when we creat the 'liked lisitings page'. If its false
 // then it will get only listings that the user has not disliked (for feed page)
-export async function getCollection(user, name, getLiked) {
+export async function getCollection(user, name, filter) {
     try {
         const querySnapshot = await getDocs(collection(firebase.db, name));
         const listings = [];
         const res = querySnapshot.forEach((doc) => { 
             const lst = doc.data()
-            if(getLiked === true) { 
-                if(lst.likes.indexOf(user.uid) === 0) {
-                    listings.push(lst) 
-                }
-            } else if(lst.dislikes.indexOf(user.uid) === -1) {
-                listings.push(lst) 
+            if(filter(lst)) { 
+                listings.push(lst)
             }
         });
         return listings;        
