36420ffc4f53623c860f0ac59d6706efa7c678af
Jonathan Swanson <john@swansonwriting.com>
2022-04-07 11:45:54 -0400

    Modified feed to show likes/dislikes

--- frontend/src/pages/feed/feed.jsx
+++ frontend/src/pages/feed/feed.jsx
@@ -30,45 +30,83 @@ class Feed extends React.Component {
   toggle = () => {
     this.setState({
       showAll: !this.state.showAll
-    })
+    });
   }
 
   dislike = (feed) => {
     const user = this.state.user;
-    // prevent duplicate entires
+
+    const idx = this.state.feeds.indexOf(feed);
+
+    // Allows for dislike & removal of dislike with same button
     if(feed.dislikes.indexOf(user.uid) === -1) {
-      feed.dislikes.push(user.uid)
-      modifyListing(feed)
+      feed.dislikes.push(user.uid);
+    } else {
+      feed.dislikes = feed.likes.filter(id => id !== this.state.user.uid);
     }
+
+    // Updates state with current feed, causing a re-render
+    this.setState({
+      ...this.state.feeds.slice(0, idx),
+      feed,
+      ...this.state.feeds.slice(idx + 1)
+    });
+    modifyListing(feed);
   }
 
   like = (feed) => {
     const user = this.state.user;
-    // prevent duplicate entries
+
+    const idx = this.state.feeds.indexOf(feed);
+
+    // Allows for like & removal of like with same button
     if(feed.likes.indexOf(user.uid) === -1) {
-      feed.likes.push(user.uid)
-      modifyListing(feed)
+      feed.likes.push(user.uid);
+    } else {
+      feed.likes = feed.likes.filter(id => id !== this.state.user.uid);
     }
+
+    // Updates state with current feed, causing a re-render
+    this.setState({
+      ...this.state.feeds.slice(0, idx),
+      feed,
+      ...this.state.feeds.slice(idx + 1)
+    });
+    modifyListing(feed);
   }
 
   render() {
     const toDisplay = this.filter(this.state.feeds);
+    
+    const feed = toDisplay.map((f) => {
+      let likeClasses = "bi bi-hand-thumbs-up-fill";
+      if(f.likes.indexOf(this.state.user.uid) !== -1) {
+        likeClasses += " liked";
+      }
 
-    const feed = toDisplay.map((f) => (
+      let dislikeClasses = "bi bi-hand-thumbs-down-fill";
+      if(f.dislikes.indexOf(this.state.user.uid) !== -1) {
+        dislikeClasses += " disliked";
+      }
+
+
+      console.log(likeClasses);
+
+      return (
         <div className="entry" key={f.desc}>
           <h4> 
-            <i className="bi bi-hand-thumbs-down-fill" onClick={() => this.dislike(f)}></i>
+            <i className={dislikeClasses} onClick={() => this.dislike(f)}></i>
             {f.name} 
-            <i className="bi bi-hand-thumbs-up-fill" onClick={() => this.like(f)}></i> </h4>
+            <i className={likeClasses} onClick={() => this.like(f)}></i> </h4>
           <div className="image-container">
             <p> {f.desc} </p>
             <hr/>
-            <ul>Skills: {
+            <ul id='skills'><h5>Skills: </h5> {
               f.skills.map(skill => <li key={skill}>{skill}</li>)  
             }</ul>
           </div>
         </div>
-    ));
+    )});
 
     return (
       <div id="feed-container">
