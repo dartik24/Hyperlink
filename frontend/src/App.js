bf4a1fe6e3cb81687bfda6ac7710b1055d07dbb5
Jonathan Swanson <john@swansonwriting.com>
2022-04-11 17:54:56 -0400

    Reverted previous change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
     if(user) {
       user = JSON.parse(user);
       this.setState({user});
-      this.props.history.push('/feed');
+      // this.props.history.push('/feed');
     }
   }
 
