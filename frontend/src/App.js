385c9117605acea6fa236d7acc2cc31ec3c251aa
Vrej <89798872+vrj1@users.noreply.github.com>
2022-04-11 01:11:49 -0400

    More UI Overhaul
    - Added animations to the navbar
    - Added animations to dislike and like buttons in the feed
    - Added hover animations to the feed skills
    - Completely revamped navbar's style
    - Some code format fixes
    - Fixed navbar spacing issue when in "employee" display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,7 +51,7 @@ class App extends React.Component {
     return (
       <div id="navbar">
         <Link to='feed'> <i className="bi bi-rss"> <p> Feed </p> </i> </Link>
-        {!this.state.user.employee ? <Link to='add-listing'> <i className="bi bi-plus-lg"> <p> Add Listing </p> </i> </Link>: <> </> }
+        {!this.state.user.employee ? <Link to='add-listing'> <i className="bi bi-plus-lg"> <p> Add Listing </p> </i> </Link>: <p class="hidden_item"> </p> }
         <Link className='right' to='/'> <i onClick={() => this.setUser(null) } className="bi bi-door-closed"> <p> Log Out </p> </i> </Link>
         <Link className='right' to='profile'> <i className="bi bi-person"> <p> Profile </p> </i> </Link>
       </div>
